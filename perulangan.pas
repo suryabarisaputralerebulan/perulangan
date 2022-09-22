d899036692640e0a8b8f8f15051eae43c4f4d9db
suryabarisaputralerebulan <suryabarisaputralerebulan@gmail.com>
2022-09-22 11:46:12 +0700

    perulangan

---
+++ perulangan.pas
@@ -0,0 +1,6 @@
+program ctk_pos;
+    Var 
+    p:integer;
+    Begin 
+    for p:= 1 to 20 do writeln('latihan jadi programmer');
+End.
---
+++ perulanganangka.pas
@@ -0,0 +1,9 @@
+Program perulanganangka;
+Var
+    x:integer;
+Begin 
+    for x:=99 downto 1 do
+    Begin
+        writeln(x);
+        End;
+End.
